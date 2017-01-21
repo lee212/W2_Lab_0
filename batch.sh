095d214e80b79150758e7d9476468b9e08b2dac7
Hyungro Lee <hroe.lee@gmail.com>
2017-01-21 09:49:37 -0500

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+if [ $# -ge 1 ]
+then
+cnt = $1
+else
+cnt = 100000
+fi
+
 while [ 1 ]
 do
-res=`time sh loop.sh 100000 2>&1`
+res=`time sh loop.sh $cnt 2>&1`
 printf "$res"|grep real
 done
